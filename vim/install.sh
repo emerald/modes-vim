4aea847441ce5a1b3211cbe7281346752dd48841
Oleks <oleks@oleks.info>
2018-01-25 12:58:17 +0100

    The year is 2018

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2016 Oleks <oleks@oleks.info>
+# Copyright (c) 2016-2018 Oleks <oleks@oleks.info>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
